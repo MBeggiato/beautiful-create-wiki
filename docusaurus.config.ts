da52fdbd96d7764e0adcdc2a4cd3010032ea5023
Marcel <marcelbeggiato@pm.me>
2025-03-21 18:59:51 +0100

    feat: update site URL and homepage title for Beautiful-Create

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   favicon: "img/BC.webp",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://bc.mbx.sh",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Beautiful-Create}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
